7a24df8830b12e030cd6ee613a733a91e75b99cc
ElDirolo <rosaldidac@gmail.com>
2022-10-14 10:15:39 +0200

    minimapa

--- Assets/Script/FirtsPerson_Controler.cs
+++ Assets/Script/FirtsPerson_Controler.cs
@@ -12,6 +12,14 @@ public class FirtsPerson_Controler : MonoBehaviour
 
     float xRotation = 0f;
 
+    bool isGrounded;
+    public Transform groundSensor;
+    public float sensorRadius;
+    public LayerMask ground;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+    Vector3 playerVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,9 +36,36 @@ public class FirtsPerson_Controler : MonoBehaviour
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90, 90);
+
         // este no funciona fpsCamera.rotation = Quaternion.Euler(mouseY, 0, 0);
         fpsCamera.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
         transform.Rotate(Vector3.up * mouseX);
-        
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        Jump();
+    }
+
+    void Jump()
+    {
+        //isGrounded = controller.isGrounded;
+        isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
+        if(isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+        }
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //playerVelocity.y += jumpForce;
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+        playerVelocity.y += gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
