aaa33b7d34f662c15bebd8896d1f07817d41bb5d
ElDirolo <rosaldidac@gmail.com>
2022-10-14 10:42:51 +0200

    minimapIcon

--- Assets/Script/MiniMapCamera.cs
+++ Assets/Script/MiniMapCamera.cs
@@ -10,5 +10,7 @@ public class MiniMapCamera : MonoBehaviour
     void LateUpdate()
     {
         transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+
+        transform.rotation = Quaternion.Euler(90, player.eulerAngles.y, 0);
     }
 }
